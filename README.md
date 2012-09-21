0d6e9e12c3db31386031f2ad844f01782fbaf6a1
Lautaro Petaccio <lausuper@gmail.com>
2012-09-21 13:41:19 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ITH-TP1
 =======
+Script python para conseguir data sobre las grabaciones.
+Necesario tener todos los archivos junto con el script en un mismo directorio.
---
+++ peum.py
@@ -0,0 +1,83 @@
+import os
+import subprocess
+
+#Devuelve la media muestral de la duracion de los segmentos con habla sobre la cantidad de fonos
+def getSpeakTimeMean(ipuList):
+	ipuList = filter(lambda x: (x[0] != '') and (x[0] != '#') and (x[0] != '# ') and (x[0] != ' '),ipuList)
+	#print ipuList
+	return reduce(lambda x, y: x+((y[2]-y[1])/len(y[0])) , ipuList, 0)
+
+#Devuelve la media muestral de la duracion de los segmentos sin sonido
+#TODO: Sacar el silencio al principio y al fin
+def getSilenceTimeMean(ipuList):
+	ipuList = filter(lambda x: (x[0] == '') or (x[0] == '#'),ipuList)
+	silenceTime = reduce(lambda x, y: x+(y[2]-y[1]) , ipuList, 0)
+	return silenceTime/len(ipuList)
+
+#Devuelve la media de F0 de los segmentos que contienen habla del archivo segun el genero, f femenino, m masculino
+def getF0Mean(fileName, gender):
+	wavFileName = fileName.split(".")[0]+".wav"
+	ipuList = filter(lambda x: (x[0] != '') and (x[0] != '#') and (x[0] != '# ') and (x[0] != ' '), getIpuData(fileName))
+	FoMean = 0
+	invalidData = 0
+	for segments in ipuList:
+		FData = callPraat(wavFileName, str(segments[1]), str(segments[2]), gender)["F0_MEAN"]
+		if FData != "--undefined--":
+			FoMean += float(FData)
+		else:
+			invalidData += 1
+	return FoMean/(len(ipuList)-invalidData)
+
+#Llama a praat con el nombre del archivo, el comienzo, el final y el genero, m hombre, f mujer
+def callPraat(fileName, begin, end, gender):
+	if gender == 'm':
+		genderBegin = 70
+		genderEnd = 300
+	else:
+		genderBegin = 100
+		genderEnd = 500
+	#print "Comando a ejecutar " + "praat acoustics.praat "+fileName+" "+begin+" "+end+" "+str(genderBegin)+" "+str(genderEnd)
+	output = subprocess.check_output("praat acoustics.praat "+fileName+" "+begin+" "+end+" "+str(genderBegin)+" "+str(genderEnd), shell=True)
+	output = output.split('\n')
+	return dict(map(lambda x: x.split(":"), output)[:-1])
+
+#Devuelve los datos de la persona que realizo el test segun el nombre del archivo
+def getTestedData(fileName):
+	global listaDatos
+	name = fileName[:-4].split('-')[0].capitalize()
+	subjectName = fileName[:-4].split('-')[1][0]
+	testNumber = fileName[:-4].split('-')[1][0]
+	gender = ""
+	nativeFrom = ""
+	age = 0
+	for item in listaDatos:
+		if item[0] == name and item[1] == subjectName:
+			gender = item[2]
+			age = int(item[3])
+		nativeFrom = item[4]
+	return {"name" : name, "subjectName" : subjectName, "gender" : gender, "city" : nativeFrom, "age" : age, "testNumber" : testNumber}
+
+#Devuelve los datos parseados en listas de los ipu
+def getIpuData(fileName):
+	file = open(fileName, 'r')
+	lista = []
+	for line in file:
+		splittedLine = line.strip('\n').split(' ', 2)
+		if len(splittedLine) > 2:
+			lista.append([splittedLine[2], float(splittedLine[0]), float(splittedLine[1])])
+	file.close()
+	return lista
+
+file = open("data.csv",'r')
+listaDatos = []
+for line in file:
+	listaDatos.append(line.replace('"', "").split('	'))
+file.close()
+
+
+#fileName = "petaccio-B1.ipu"
+for fileName in os.listdir("."):
+	if ".ipu" in fileName:
+		#print getSpeakTimeMean(getIpuData(fileName))
+		print fileName
+		print getF0Mean(fileName, getTestedData(fileName)["gender"])
